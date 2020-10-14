bd06c2d947a52fb16282719a037dc62d5b3dc4ea
Alexandre Sobota <alexandre.sobota@hotmail.com>
2020-10-14 20:12:40 -0300

    fazendo front-end

--- web/index.js
+++ web/index.js
@@ -1,3 +1,4 @@
+// Toggle para dispositivos menores
 $(function () {
     $(".toggle").on("click", function () {
         if ($(".item").hasClass("active")) {
@@ -8,6 +9,7 @@ $(function () {
     });
 });
 
+// Menu retrátil Destinos
 $(".upper").click(function () {
 	// Adding a class to the upper list item to handle the icon animation and background change
 	$(this).toggleClass("collapsed");
@@ -23,3 +25,48 @@ $(".upper").click(function () {
 		collapsedMenu.style.maxHeight = collapsedMenu.scrollHeight + "px";
 	}
 });
+
+// Modal
+function onOff() {
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
+}
+
+function checkFields(event) {
+    
+    const valuesToCheck = [
+        "title",
+        "category",
+        "image",
+        "description",
+        "link",
+    ]
+
+    const isEmpty = valuesToCheck.find(function(value) {
+
+        const checkIfIsString = typeof event.target[value].value === "string";
+        const checkIfIsEmpty = !event.target[value].value.trim()
+        
+        if(checkIfIsString && checkIfIsEmpty) {
+            return true
+        }
+    })
+
+    if(isEmpty) {
+        event.preventDefault()
+        alert("Por favor, preencha todos os campos!");
+    }
+
+}
