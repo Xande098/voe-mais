fdb7c20e5ccd5219794492fc5c13dddea595b3bd
Alexandre Sobota <alexandre.sobota@hotmail.com>
2020-11-02 23:44:16 -0300

    tentando conexão

--- cadastroUsers.php
+++ cadastroUsers.php
@@ -5,219 +5,222 @@
     <h1>Tela de Cadastro</h1>
 
     <!-- <form action="/" method="POST" onsubmit="return checkFields(event)"> -->
-    <form action="cadastroUsers_exe.php" method="post" onsubmit="return check(this.form)">
+    <div class="não-sei">
+      <form action="cadastroUsers_exe.php" method="post" onsubmit="return check(this.form)">
 
-      <input type="hidden" id="acaoForm" name="acaoForm" value="Contratar">
-    
-      <div class="input-wrapper">
-        <label for="nomeCliente">Nome</label>
-        <input
-          name="nomeCliente"
-          type="text" 
-          pattern="[a-zA-Z\u00C0-\u00FF ]{3,15}$"
-          title="Nome entre 3 e 15 letras."
-          placeholder="Joe"
-          required
-        >
-      </div>
+        <input type="hidden" id="acaoForm" name="acaoForm" value="Contratar">
+      
+        <div class="input-wrapper">
+          <label for="nomeCliente">Nome</label>
+          <input
+            name="nomeCliente"
+            type="text" 
+            pattern="[a-zA-Z\u00C0-\u00FF ]{3,15}$"
+            title="Nome entre 3 e 15 letras."
+            placeholder="Joe"
+            required
+          >
+        </div>
 
-      <div class="input-wrapper">
-        <label for="sobrenomeCliente">Sobrenome</label>
-        <input
-          name="sobrenomeCliente"
-          type="text" 
-          pattern="[a-zA-Z\u00C0-\u00FF ]{3,15}$"
-          title="Sobrenome entre 3 e 15 letras."
-          placeholder="Travel"
-          required
-        >
-      </div>
+        <div class="input-wrapper">
+          <label for="sobrenomeCliente">Sobrenome</label>
+          <input
+            name="sobrenomeCliente"
+            type="text" 
+            pattern="[a-zA-Z\u00C0-\u00FF ]{3,15}$"
+            title="Sobrenome entre 3 e 15 letras."
+            placeholder="Travel"
+            required
+          >
+        </div>
 
-      <div class="input-wrapper">
-        <label for="cpf_cliente">CPF</label>
-        <input
-          name="cpf_cliente"
-          type="text"
-          pattern="^(\d{3}\.\d{3}\.\d{3}-\d{2})|(\d{11})$"
-          placeholder="888.888.888-88"
-          title="Digite o cpf corretamente."
-          required
-        >
-      </div>
+        <div class="input-wrapper">
+          <label for="cpf_cliente">CPF</label>
+          <input
+            name="cpf_cliente"
+            type="text"
+            pattern="^(\d{3}\.\d{3}\.\d{3}-\d{2})|(\d{11})$"
+            placeholder="888.888.888-88"
+            title="Digite o cpf corretamente."
+            required
+          >
+        </div>
 
-      <div class="input-wrapper">
-        <label for="DataNasc">Data de Nascimento</label>
-        <input
-          name="DataNasc"
-          type="date"
-          pattern="((0[1-9])|([1-2][0-9])|(3[0-1]))\/((0[1-9])|(1[0-2]))\/((19|20)[0-9][0-9])"
-          title="Formato: dd/mm/aaaa."
-          required
-        >
-      </div>
+        <div class="input-wrapper">
+          <label for="DataNasc">Data de Nascimento</label>
+          <input
+            name="DataNasc"
+            type="date"
+            pattern="((0[1-9])|([1-2][0-9])|(3[0-1]))\/((0[1-9])|(1[0-2]))\/((19|20)[0-9][0-9])"
+            title="Formato: dd/mm/aaaa."
+            required
+          >
+        </div>
 
-      <div class="input-wrapper">
-        <label for="email">E-mail</label>
-        <input
-          type="email"
-          name="email"
-          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-          title="Digite um e-mail válido."
-          placeholder="joe@example.com"
-          required
-        >
-      </div>
+        <div class="input-wrapper">
+          <label for="email">E-mail</label>
+          <input
+            type="email"
+            name="email"
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+            title="Digite um e-mail válido."
+            placeholder="joe@example.com"
+            required
+          >
+        </div>
 
-      <div class="input-wrapper">
-        <label for="nomePais">País</label>
-        <input
-          type="text"
-          name="nomePais" 
-          pattern="[a-zA-Z\u00C0-\u00FF ]{2,15}$"
-          title="Nome do País de 2 à 15 letras."
-          placeholder="Brasil"
-          required       
-        >
-      </div>
+        <div class="input-wrapper">
+          <label for="nomePais">País</label>
+          <input
+            type="text"
+            name="nomePais" 
+            pattern="[a-zA-Z\u00C0-\u00FF ]{2,15}$"
+            title="Nome do País de 2 à 15 letras."
+            placeholder="Brasil"
+            required       
+          >
+        </div>
 
-      <div class="input-wrapper">
-        <label for="tipoTel">Tipo de Telefone</label>
-        <input
-          list="tipo-tel"
-          type="text"
-          name="tipoTel"
-          title="Escolha uma das opções disponíveis."
-          placeholder="Escolha uma das opções disponíveis."
-          required
-        >
-        <datalist id="tipo-tel">
-          <option value="Celular">
-          <option value="Trabalho">
-          <option value="Casa">
-          <option value="Recado">
-        </datalist>
-      </div>
+        <div class="input-wrapper">
+          <label for="tipoTel">Tipo de Telefone</label>
+          <input
+            list="tipo-tel"
+            type="text"
+            name="tipoTel"
+            title="Escolha uma das opções disponíveis."
+            placeholder="Escolha uma das opções disponíveis."
+            required
+          >
+          <datalist id="tipo-tel">
+            <option value="Celular">
+            <option value="Trabalho">
+            <option value="Casa">
+            <option value="Recado">
+          </datalist>
+        </div>
 
-      <div class="input-wrapper">
-        <label for="title">Telefone</label>
-        <input
-          type="tel"
-          name="numTel"
-          maxlength="15"
-          pattern="\([0-9]{2}\) [0-9]{4,6}-[0-9]{3,4}$"
-          title="(41) 99999-9999"
-          placeholder="(41) 99999-9999"
-        >
-      </div>
+        <div class="input-wrapper">
+          <label for="numTel">Telefone</label>
+          <input
+            type="tel"
+            name="numTel"
+            maxlength="15"
+            pattern="\([0-9]{2}\) [0-9]{4,6}-[0-9]{3,4}$"
+            title="(41) 99999-9999"
+            placeholder="(41) 99999-9999"
+            required
+          >
+        </div>
 
-      <div class="input-wrapper">
-        <label for="nomeEstado">Estado</label>
-        <input
-          type="text"
-          name="nomeEstado"
-          pattern="[a-zA-Z\u00C0-\u00FF ]{2,15}$"
-          title="Nome do Estado de 2 à 15 letras."
-          placeholder="Paraná"
-          required  
-        >
-      </div>
+        <div class="input-wrapper">
+          <label for="nomeEstado">Estado</label>
+          <input
+            type="text"
+            name="nomeEstado"
+            pattern="[a-zA-Z\u00C0-\u00FF ]{2,15}$"
+            title="Nome do Estado de 2 à 15 letras."
+            placeholder="Paraná"
+            required  
+          >
+        </div>
 
-      <div class="input-wrapper">
-        <label for="nomeCidade">Cidade</label>
-        <input
-          type="text"
-          name="nomeCidade"
-          pattern="[a-zA-Z\u00C0-\u00FF ]{2,15}$"
-          title="Nome do Cidade de 2 à 15 letras."
-          placeholder="Curitiba"
-          required  
-        >
-      </div>
+        <div class="input-wrapper">
+          <label for="nomeCidade">Cidade</label>
+          <input
+            type="text"
+            name="nomeCidade"
+            pattern="[a-zA-Z\u00C0-\u00FF ]{2,15}$"
+            title="Nome do Cidade de 2 à 15 letras."
+            placeholder="Curitiba"
+            required  
+          >
+        </div>
 
-      <div class="input-wrapper">
-        <label for="tipoLogradouro">Tipo Logradouro</label>
-        <input
-          type="text"
-          name="tipoLogradouro"
-          list="tipo-Logradouro"
-          title="Escolha uma das opções disponíveis."
-          placeholder="Escolha uma das opções disponíveis."
-          required
-        >
-        <datalist id="tipo-Logradouro">
-          <option value="Rua">
-          <option value="Avenida">
-        </datalist>
-      </div>
+        <div class="input-wrapper">
+          <label for="tipoLogradouro">Tipo Logradouro</label>
+          <input
+            type="text"
+            name="tipoLogradouro"
+            list="tipo-Logradouro"
+            title="Escolha uma das opções disponíveis."
+            placeholder="Escolha uma das opções disponíveis."
+            required
+          >
+          <datalist id="tipo-Logradouro">
+            <option value="Rua">
+            <option value="Avenida">
+          </datalist>
+        </div>
 
-      <div class="input-wrapper">
-        <label for="nomeRua">Logradouro</label>
-        <input
-          type="text"
-          name="nomeRua"
-          pattern="[a-zA-Z\u00C0-\u00FF ]{5,30}$"
-          title="Digite o endereço de 5 à 30 letras."
-          placeholder="Eleutério de Souza Padilha"
-          required  
-        >
-      </div>
+        <div class="input-wrapper">
+          <label for="nomeRua">Logradouro</label>
+          <input
+            type="text"
+            name="nomeRua"
+            pattern="[a-zA-Z\u00C0-\u00FF ]{5,30}$"
+            title="Digite o endereço de 5 à 30 letras."
+            placeholder="Eleutério de Souza Padilha"
+            required  
+          >
+        </div>
 
-      <div class="input-wrapper">
-        <label for="numRua">Número</label>
-        <input
-          type="number"
-          name="numRua"
-          oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
-          maxlength = "7"
-          title="Digite o número do seu endereço."
-          placeholder="1004"
-          required  
-        >
-      </div>
+        <div class="input-wrapper">
+          <label for="numRua">Número</label>
+          <input
+            type="number"
+            name="numRua"
+            oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
+            maxlength = "7"
+            title="Digite o número do seu endereço."
+            placeholder="1004"
+            required  
+          >
+        </div>
 
-      <div class="input-wrapper">
-        <label for="nomeBairro">Bairro</label>
-        <input
-          type="text"
-          name="nomeBairro"
-          pattern="[a-zA-Z\u00C0-\u00FF ]{5,30}$"
-          title="Digite o Bairro de 5 à 30 letras."
-          placeholder="Centro"
-          required  
-        >
-      </div>
+        <div class="input-wrapper">
+          <label for="nomeBairro">Bairro</label>
+          <input
+            type="text"
+            name="nomeBairro"
+            pattern="[a-zA-Z\u00C0-\u00FF ]{5,30}$"
+            title="Digite o Bairro de 5 à 30 letras."
+            placeholder="Centro"
+            required  
+          >
+        </div>
 
-      <div class="input-wrapper">
-        <label for="senha">Senha</label>
-        <input
-          type="password"
-          name="senha"
-          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,8}"
-          title="Deve conter ao menos um número, uma letra maiúscula e minúscula, um caracter especial, com 6 a 8 caracteres"
-          required
-        >
-      </div>
+        <div class="input-wrapper">
+          <label for="senha">Senha</label>
+          <input
+            type="password"
+            name="senha"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,8}"
+            title="Deve conter ao menos um número, uma letra maiúscula e minúscula, um caracter especial, com 6 a 8 caracteres"
+            required
+          >
+        </div>
 
-      <div class="input-wrapper">
-        <label for="senha2">Confirme a Senha</label>
-        <input
-          type="password"
-          name="senha2"
-          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,8}"
-          title="Deve conter ao menos um número, uma letra maiúscula e minúscula, um caracter especial, com 6 a 8 caracteres"
-          required
-        >
-      </div>
+        <div class="input-wrapper">
+          <label for="senha2">Confirme a Senha</label>
+          <input
+            type="password"
+            name="senha2"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,8}"
+            title="Deve conter ao menos um número, uma letra maiúscula e minúscula, um caracter especial, com 6 a 8 caracteres"
+            required
+          >
+        </div>
 
-      <!-- <a href="." onclick="onOffSignUp()">Voltar</a>
-      <button>Salvar</button> -->
-      <!-- <input type="button" value="Voltar" class="bt-signup-voltar" onclick="window.location.href='.'"> -->
+        <!-- <a href="." onclick="onOffSignUp()">Voltar</a>
+        <button>Salvar</button> -->
+        <!-- <input type="button" value="Voltar" class="bt-signup-voltar" onclick="window.location.href='.'"> -->
 
-      <input type="button" value="Voltar" class="bt-signup-voltar" onclick="onOffSignUp()">
-      <input type="submit" value="Salvar" class="bt-signup-salvar" >
+        <input type="button" value="Voltar" class="bt-signup-voltar" onclick="onOffSignUp()">
+        <input type="submit" value="Salvar" class="bt-signup-salvar" >
 
 
-    </form>
+      </form>
+    </div>
 
   </div>
 </div>
--- cadastroUsers_exe.php
+++ cadastroUsers_exe.php
@@ -13,33 +13,33 @@ echo "</p> " */
 <?php
 
 $servername = "localhost:3307";
-/* $username = "usu@IE_Exe";
-$password = "php@PUCPR"; */
+$username = "userVoe";
+$password = "1234";
 $database = "voemais";
 
 
 $tipoTel = $_POST['tipoTel'];
 $celular = $_POST['numTel'];
-$id_tipoTel = $_POST['id_tipoTel'];
+/* $id_tipoTel = $_POST['id_tipoTel']; */
 $tipoLogradouro = $_POST['tipoLogradouro'];
 $nomeBairro = $_POST['nomeBairro'];
 $nomeCidade = $_POST['nomeCidade'];
 $nomeEstado = $_POST['nomeEstado'];
-$id_cidade = $_POST['id_cidade'];
+/* $id_cidade = $_POST['id_cidade']; */
 $nomePais = $_POST['nomePais'];
-$id_estado = $_POST['id_estado'];
+/* $id_estado = $_POST['id_estado']; */
 $nomeRua = $_POST['nomeRua'];
 $numRua = $_POST['numRua'];
-$id_logradouro = $_POST['id_logradouro'];
-$id_bairro = $_POST['id_bairro'];
-$id_pais = $_POST['id_pais'];
+/* $id_logradouro = $_POST['id_logradouro']; */
+/* $id_bairro = $_POST['id_bairro']; */
+/* $id_pais = $_POST['id_pais']; */
 $login   = $_POST['cpf_cliente'];
 $nomeCliente = $_POST['nomeCliente'];
 $sobrenomeCliente = $_POST['sobrenomeCliente'];
 $email = $_POST['email'];
 $dtNasc  = $_POST['DataNasc'];
-$id_endereco  = $_POST['id_endereco'];
-$id_telefone  = $_POST['id_telefone'];
+/* $id_endereco  = $_POST['id_endereco']; */
+/* $id_telefone  = $_POST['id_telefone']; */
 $acao    = $_POST['acaoForm'];
 
 if ($dtNasc != "") {
@@ -61,7 +61,7 @@ if ($dtNasc != "") {
 $md5Senha = md5($_POST['senha']);
 
 // Cria conexão
-$conn = mysqli_connect($servername, $database);
+$conn = mysqli_connect($servername, $username, $password, $database);
 /* $conn = mysqli_connect($servername, $username, $password, $database); */
 
 // Verifica conexão
@@ -78,18 +78,43 @@ mysqli_query($conn, 'SET character_set_results=utf8');
 
 // Tipo de telefone pode ser: 'Celular', 'Trabalho', 'Casa', 'Recado'
 // Faz Select na Base de Dados
-$sql = "INSERT INTO tipo_telefone (tipoTel) VALUES ('$tipoTel')";
-$sql = "INSERT INTO telefone (numTel, id_tipoTel) VALUES ('$celular','$id_tipoTel')";
-$sql = "INSERT INTO tipo_logradouro (tipoLogradouro) VALUES ('$tipoLogradouro')";
-$sql = "INSERT INTO bairro (nomeBairro) VALUES ('$nomeBairro')";
-$sql = "INSERT INTO cidade (nomeCidade) VALUES ('$nomeCidade')";
-$sql = "INSERT INTO estado (nomeEstado, id_cidade) VALUES ('$nomeEstado', '$id_cidade')";
-$sql = "INSERT INTO pais (nomePais, id_estado) VALUES ('$nomePais', '$id_estado')";
-$sql = "INSERT INTO endereco (nomeRua, numRua, id_logradouro, id_bairro, id_pais) VALUES ('$nomeRua', '$numRua', '$id_logradouro', '$id_bairro', '$id_pais')";
-$sql = "INSERT INTO cliente (cpf_cliente, nomeCliente, sobrenomeCliente, email, dataNasc, senha, id_endereco, id_telefone) VALUES ('$login','$nomeCliente','$sobrenomeCliente','$email','$dtNasc', '$md5Senha', '$id_endereco', '$id_telefone')";
+$sql = "INSERT INTO tipo_telefone (default, tipoTel) VALUES ('$tipoTel')";
+/* $sql = "INSERT INTO telefone (numTel, id_tipoTel) VALUES ('$celular','$id_tipoTel')"; */
+$sql = "INSERT INTO telefone (default, id_tipoTel, numTel) VALUES (
+  (SELECT MAX(id_tipoTel) FROM tipo_telefone),
+  '$celular')";
+$sql = "INSERT INTO tipo_logradouro (default, tipoLogradouro) VALUES ('$tipoLogradouro')";
+$sql = "INSERT INTO bairro (default, nomeBairro) VALUES ('$nomeBairro')";
+$sql = "INSERT INTO cidade (default, nomeCidade) VALUES ('$nomeCidade')";
+/* $sql = "INSERT INTO estado (nomeEstado, id_cidade) VALUES ('$nomeEstado', '$id_cidade')"; */
+$sql = "INSERT INTO estado (default, id_cidade, nomeEstado) VALUES (
+  (SELECT MAX(id_cidade) FROM cidade),
+  '$nomeEstado')";
+/* $sql = "INSERT INTO pais (nomePais, id_estado) VALUES ('$nomePais', '$id_estado')"; */
+$sql = "INSERT INTO pais (default, id_estado, nomePais) VALUES (
+  (SELECT MAX(id_estado) FROM estado),
+  '$nomePais')";
+/* $sql = "INSERT INTO endereco (nomeRua, numRua, id_logradouro, id_bairro, id_pais) VALUES ('$nomeRua', '$numRua', '$id_logradouro', '$id_bairro', '$id_pais')"; */
+$sql = "INSERT INTO endereco (default, id_logradouro, id_bairro, id_pais, nomeRua, numRua) VALUES (
+  (SELECT MAX(id_logradouro) FROM tipo_logradouro),
+  (SELECT MAX(id_bairro) FROM bairro),
+  (SELECT MAX(id_pais) FROM pais),
+  '$nomeRua',
+  '$numRua')";
+/* $sql = "INSERT INTO cliente (cpf_cliente, nomeCliente, sobrenomeCliente, email, dataNasc, senha, id_endereco, id_telefone) VALUES ('$login','$nomeCliente','$sobrenomeCliente','$email','$dtNasc', '$md5Senha', '$id_endereco', '$id_telefone')"; */
+$sql = "INSERT INTO cliente (id_endereco, id_telefone, cpf_cliente, nomeCliente, sobrenomeCliente, email, dataNasc, senha) VALUES (
+  (SELECT MAX(id_endereco) FROM endereco),
+  (SELECT MAX(id_telefone) FROM telefone),
+  '$login',
+  '$nomeCliente',
+  '$sobrenomeCliente',
+  '$email',
+  '$dtNasc',
+  '$md5Senha')";
 /* $sql = "INSERT INTO professor (Nome, Celular, DataNasc, Login, Senha) VALUES ('$nome','$celular','$dtNasc','$login','$md5Senha')"; */
 
 if ($result = mysqli_query($conn, $sql)) {
+  echo "<div class='não-sei'>";
   if ($acao == "Contratar")
     echo "Um registro adicionado!";
   else
